2cfa4fa4d0c41cc92d493c2ca6845074ceb24ac7
alr_dev <157879926+Alr-Dev@users.noreply.github.com>
2024-11-06 16:58:39 -0300

    Create mpsmatchpitch.lua

---
+++ mpsmatchpitch.lua
@@ -0,0 +1,23 @@
+
+if _G.BypassTwo or _G.BypassedOne then
+    print('Bypass was alredy executed.')
+end
+if _G.ScriptExec then
+    return print('Script was alredy executed.')
+end
+local types = {
+    X = game:GetService("ReplicatedStorage").Events.AntiCheat,
+    V = game:GetService("StarterPlayer").StarterPlayerScripts.Scripts.AntiCheat
+}
+_G.BypassedOne = true
+_G.BypassTwo = true
+_G.ScriptExec = true
+local function bypass()
+if types.V then
+    types.V:Destroy()
+end
+if types.X then
+    types.X:Destroy()
+end
+end
+bypass()
