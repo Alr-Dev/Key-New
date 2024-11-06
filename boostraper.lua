edb8b87d72f40bd217f57ef1b47f867fd8ff9744
alr_dev <157879926+Alr-Dev@users.noreply.github.com>
2024-11-06 17:35:48 -0300

    Update boostraper.lua

--- boostraper.lua
+++ boostraper.lua
@@ -1,5 +1,10 @@
 
-
-if game.Players.LocalPlayer.Name == 'nasciem1926ok' then
+if game.Players.LocalPlayer.Name == 'nasciem1926ok' or 'Madaratrror' then
+if game.PlaceId == 95304903292658 then
+ return loadstring(game:HttpGet(('https://raw.githubusercontent.com/Alr-Dev/Key-New/refs/heads/main/loadfile3.lua')))()
+end
+if game.PlaceId == 9847297509 then
+ return loadstring(game:HttpGet(('https://github.com/Alr-Dev/Key-New/edit/main/loadfile2.lua')))()
+end
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Alr-Dev/Key-New/refs/heads/main/loadfile.lua')))()
 end
