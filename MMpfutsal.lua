99bad5d843ee8a2f5f070ba7ba4845d054d61737
alr_dev <157879926+Alr-Dev@users.noreply.github.com>
2024-11-06 17:29:10 -0300

    Update MMpfutsal.lua

--- MMpfutsal.lua
+++ MMpfutsal.lua
@@ -6,3 +6,23 @@ local types = {
   Remote = game:GetService("ReplicatedStorage").Events.AntiCheat
 }
 types.Remote:Destroy()
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+local window = DrRayLibrary:Load("SPJ Reach", "Default")
+local Tab = DrRayLibrary.newTab("Main", "ImageIdHere")
+local Tab2 = DrRayLibrary.newTab("Customize", "ImageIdHere")
+Tab.newSlider("Right leg", "", 100, false, function(Value) 
+    local rightLeg = game.Players.LocalPlayer.Character['Right Leg']
+    rightLeg.Size = Vector3.new(Value, rightLeg.Size.Y, Value)
+end)
+Tab.newSlider("Left leg", "", 100, false, function(Value) 
+    local leftLeg = game.Players.LocalPlayer.Character['Left Leg']
+    leftLeg.Size = Vector3.new(Value, leftLeg.Size.Y, Value)
+end)
+Tab2.newSlider("Right leg transparency", "", 5, false, function(Value) 
+    game.Players.LocalPlayer.Character['Right Leg'].Transparency = Value
+end)
+Tab2.newSlider("Left leg transparency", "", 5, false, function(Value) 
+    game.Players.LocalPlayer.Character['Left Leg'].Transparency = Value
+end)
+game.Players.LocalPlayer.Character['Right Leg'].Massless = true
+game.Players.LocalPlayer.Character['Left Leg'].Massless = true
